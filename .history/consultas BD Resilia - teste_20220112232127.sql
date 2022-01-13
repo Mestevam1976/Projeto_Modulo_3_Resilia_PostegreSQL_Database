5f79910473d408e7a5b05915cf75092223c42aba
Mestevam1976 <marcio.estevam@yahoo.com>
2022-01-12 23:21:56 -0300

    Consultas iniciais BD Resilia

---
+++ .history/consultas BD Resilia - teste_20220112232127.sql
@@ -0,0 +1,26 @@
+
+--SELECIONAR A QUANTIDADE DE ALUNOS CADASTRADOS NO BANCO
+SELECT COUNT(alunos.nome_aluno) FROM alunos
+
+--SELECIONAR TODOS OS ALUNOS E OS CURSOS EM QUE ESTÃO CADASTRADOS
+SELECT alunos.nome_aluno, cursos.nome_curso FROM alunos 
+JOIN cursos
+ON cursos.id_curso = alunos.id_curso
+GROUP BY alunos.nome_aluno, cursos.nome_curso 
+ORDER BY cursos.nome_curso
+
+--SELECIONAR QUAIS PESSOAS FACILITADORAS ATUAM EM MAIS DE UMA TURMA
+SELECT alunos.turma, facilitadores.nome_facilitador FROM alunos 
+JOIN facilitadores
+ON facilitadores.id_facilitador = alunos.id_facilitador 
+GROUP BY alunos.turma, facilitadores.nome_facilitador 
+ORDER BY COUNT(facilitadores.nome_facilitador) >=2
+
+--Exemplo de consulta com 03 tabelas diferentes
+SELECT alunos.nome_aluno, facilitadores.nome_facilitador, cursos.nome_curso FROM alunos 
+JOIN facilitadores
+ON facilitadores.id_facilitador = alunos.id_facilitador 
+JOIN cursos
+ON cursos.id_curso = alunos.id_curso
+GROUP BY alunos.nome_aluno, facilitadores.nome_facilitador, cursos.nome_curso 
+ORDER BY cursos.nome_curso
